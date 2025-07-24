88b7ba391193fc517a2324705eb1e858277fa301
tejasjadhav06 <jadhavtejas405@gmail.com>
2025-07-24 20:42:14 +0530

    Updated Favicon.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom"; // ✅ ADD THIS
+import { BrowserRouter } from "react-router-dom"; 
 
 import "./index.css";
 
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       {" "}
-      {/* ✅ WRAP App inside BrowserRouter */}
+      {/*  */}
       <ThemeProvider>
         <App />
       </ThemeProvider>
